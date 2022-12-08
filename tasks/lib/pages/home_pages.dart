78051e321a12e63aa1a3e33ba9eb856c707a0b3b
HerbertVelasco <havm14@gmail.com>
2022-12-07 22:52:39 -0500

    1er cambio

--- tasks/lib/pages/home_pages.dart
+++ tasks/lib/pages/home_pages.dart
@@ -1,10 +1,81 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  CollectionReference tasksReference =
+      FirebaseFirestore.instance.collection('tasks');
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Firebase"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                tasksReference.get().then((QuerySnapshot value) {
+                  // QuerySnapshot collection = value;
+                  // List<QueryDocumentSnapshot> docs = collection.docs;
+                  // QueryDocumentSnapshot doc = docs[0];
+                  QuerySnapshot collection = value;
+                  collection.docs.forEach((QueryDocumentSnapshot element) {
+                    Map<String, dynamic> myMap =
+                        element.data() as Map<String, dynamic>;
+                  });
+                });
+              },
+              child: Text(
+                "Obtener Data",
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                tasksReference
+                    .add({
+                      "title": "Ir de compras al super",
+                      "description":
+                          "Debemos de comprar comida para todo el mes",
+                    })
+                    .then((DocumentReference value) {})
+                    .catchError((error) {
+                      print("Ocurrio un error en el registro");
+                    })
+                    .whenComplete(() {
+                      print("El registro a terminado");
+                    });
+              },
+              child: Text(
+                "Agregar",
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                tasksReference.doc("").update(
+                  {
+                    "title": "Ir de paseo",
+                    "description": "tenemos que salir muy temprao",
+                  },
+                ).catchError(
+                  (error) {
+                    print(error);
+                  },
+                ).whenComplete(
+                  () {
+                    print("Actualizacion terminada");
+                  },
+                );
+              },
+              child: Text(
+                "Actualizar documento",
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
